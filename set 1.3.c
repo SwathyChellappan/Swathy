3414987584dd5c43070dca37e8af3b53b49309dc
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-15 21:02:47 +0530

    Create set 1.3.c

---
+++ set 1.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='3'){
+printf("not valid");}
+else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+    printf("vowels");}
+    else{
+    printf("consonents");}
+
+    
+}
