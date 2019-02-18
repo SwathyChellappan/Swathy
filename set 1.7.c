f0b70bb5d7c24193f12eb7d2ad5048756bb54739
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-18 15:52:46 +0530

    Create set 1.7.c

---
+++ set 1.7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("\nenter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    printf("\nHello");
+    }
+    
+
+    return 0;
+}
