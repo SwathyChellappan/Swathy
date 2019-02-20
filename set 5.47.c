75625bc5d9c7cd4e900086fa516eccaad14ea03e
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-20 23:06:27 +0530

    Create set 5.47.c

---
+++ set 5.47.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[50],i,j,k,t;
+    printf("enter the limit");
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i-0;i<k;i++)
+    {
+    for(j=i+1;j<k;j++)
+    {
+        if(a[i]>a[j])
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+        
+    }
+    
+}
+    for(i=0;i<k;i++)
+    {
+        printf("%d",a[i]);
+        
+    }
+    printf("smallest number %d",a[0]);
+    printf("largest number %d",a[k-1]);
+
+    return 0;
+}
