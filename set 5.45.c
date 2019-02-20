4886cc73303570122fa90276defe9d5e706ae809
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-20 22:47:34 +0530

    Create set 5.45.c

---
+++ set 5.45.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,c=0,n1;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        n1=n%10;
+        c++;
+        n=n/10;
+        
+    }
+    printf("%d",c);
+
+    return 0;
+}
