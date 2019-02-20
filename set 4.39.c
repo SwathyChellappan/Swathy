b27238a312eff3af54af60d776b69951d3a6134f
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-20 19:49:00 +0530

    Create set 4.39.c

---
+++ set 4.39.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[20],i,temp;
+    printf("enter number");
+    for(i=1;i<=10;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    temp=a[1];
+    for(i=1;i<=10;i++)
+    {
+        if(temp<a[i])
+        {
+            temp=a[i];
+        }
+    }
+
+printf("%d",temp);
+    return 0;
+}
