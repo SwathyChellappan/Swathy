1c2f3cc238f5e80ca9a4355c71f82645322525c5
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-23 22:57:47 +0530

    Create set 7.61.py

---
+++ set 7.61.py
@@ -0,0 +1,13 @@
+def firstk():
+    str=input()
+    s=''
+    k=int(input())
+    for i in range(0,k):
+        s+=str[i]
+    print(s)
+try:
+    firstk()
+except:
+    print('invalid')
+main()
+        
