20c06633684ce9f1c070e4cacbba1640a72a45ee
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-23 22:09:27 +0530

    Create set 6.55.py

---
+++ set 6.55.py
@@ -0,0 +1,15 @@
+def oddeven():
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+def mul2():
+    try:
+        m=int(input())
+        n=int(input())
+        oddeven(m*n)
+    except:
+        print('invalid')
+        
+    
+        
