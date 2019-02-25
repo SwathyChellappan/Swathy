b81776eef19e1291145b7b5bbba3f114385bd552
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-25 20:03:44 +0530

    Create set 7.66.py

---
+++ set 7.66.py
@@ -0,0 +1,15 @@
+def main():
+    x=int(input())
+    flag=0
+    i=2<x
+    for i in range:
+        if x%i==0:
+            flag+=1 
+            break
+        if flag==0:
+            print('not a prime')
+        else:
+            print('prime')
+        
+        
+            
