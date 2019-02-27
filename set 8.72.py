b3683b3ebb98ffd9dd5e789a9aa594be624c3769
SwathyChellappan <47550495+SwathyChellappan@users.noreply.github.com>
2019-02-27 23:03:38 +0530

    Create set 8.72.py

---
+++ set 8.72.py
@@ -0,0 +1,6 @@
+def main():
+    str1=int(input())
+    if str1=='a'or str1=='e'or str1=='i'or str=='o'or str1=='u'or str=='A'or str1=='E'or str1=='I'or str1=='O'or str1=='u':
+        print('yes')
+    else:
+        print('no')
